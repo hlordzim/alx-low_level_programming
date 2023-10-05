f5b03dc4363faaca30c4977d24f27eb91e2817ca
hlordzim <hlordzim.mh@gmail.com>
2023-10-05 15:30:17 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function serves as the entry point for the program. It initializes
+ * the application and performs some tasks.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
