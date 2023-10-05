0e0e768b14f10dae08ef3b4e498ab032e2cee207
hlordzim <hlordzim.mh@gmail.com>
2023-10-05 16:25:49 -0700

    task 6 sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 printf("Size of char: %lu bytes\n", sizeof(char));
 printf("Size of int: %lu bytes\n", sizeof(int));
-printf("Size of long: %lu bytes\n", sizeof(long));
+printf("Size of long int: %lu bytes\n", sizeof(long int));
 printf("Size of float: %lu bytes\n", sizeof(float));
-printf("Size of long long: %lu bytes\n", sizeof(long long));
+printf("Size of long long int: %lu bytes\n", sizeof(long long int));
 return (0);
 }
