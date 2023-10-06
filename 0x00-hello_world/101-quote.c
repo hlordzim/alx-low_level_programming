c96e5658b6b67cf94593e4d05cf81a6d23471c68
hlordzim <hlordzim.mh@gmail.com>
2023-10-05 17:45:05 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n");
+const char *message =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, 60);
 return (1);
 }
