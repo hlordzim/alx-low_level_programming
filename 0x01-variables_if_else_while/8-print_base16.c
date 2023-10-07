d7413dd254835e089cd47d0b62661bde4c3e7710
hlordzim <hlordzim.mh@gmail.com>
2023-10-06 20:38:35 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always Return 0
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	for (digit = 'a'; digit <= 'f'; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
