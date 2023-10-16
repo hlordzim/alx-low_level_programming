fb8f0a988f61bda17afaa42032428b3f6e6e35c8
hlordzim <hlordzim.mh@gmail.com>
2023-10-16 15:36:57 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Variable for the function int *n
+ *
+ * main - check the code
+ * @n: Variable to update the value it point to 98
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# C - Pointers, arrays and strings
+
+This is a README.md file for C_pointers, arrays and strings
