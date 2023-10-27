e8b00a2ab1cf1e6825563cbc2110d4fa0a793a1b
hlordzim <hlordzim.mh@gmail.com>
2023-10-27 12:15:30 -0700

    task 0 argc,argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("My name is: %s\n", argv[0]);
+	}
+	return 0;
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A. C - argc, argv
+
+This file contain the poject and / task for argc, argv.
