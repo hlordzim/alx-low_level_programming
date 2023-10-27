ecfe1d68567c2f566e30d2baba3421aad03700ff
hlordzim <hlordzim.mh@gmail.com>
2023-10-27 12:39:31 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main -  program that prints its name, followed by a new line
+ * @argc: argument count to hold integer value
+ * @argv: argument vector to represent one of command line argument
+ *
+ * Return: Always 0 (success)
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("My name is: %s\n", argv[0]);
+		printf("mynameis %s\n", argv[0]);
 	}
-	return 0;
+	return (0);
 }
