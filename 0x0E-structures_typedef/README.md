342c66254c441717cd838aa781ce9440da62ea07
hlordzim <hlordzim.mh@gmail.com>
2023-11-06 15:05:40 -0800

    task 0 of Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+# ALX 0x0E. C - Structures, typedef
+
+typedef
+The C programming language provides a keyword called typedef, which you can use to give a type, a new name.
+
+You can also use typedef with structures to define a new data type.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure declaration for dog.
+ * @name: name of a dog.
+ * @age: age of a dog
+ * @owner: owner of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
