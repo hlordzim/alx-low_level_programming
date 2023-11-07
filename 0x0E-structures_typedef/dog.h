cb2b118beaed0e7945806ca392919f78646aa748
hlordzim <hlordzim.mh@gmail.com>
2023-11-07 08:28:37 -0800

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include "dog.h"
 #include <stddef.h>
 
 /**
@@ -16,6 +15,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include "dog.h"
+
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
