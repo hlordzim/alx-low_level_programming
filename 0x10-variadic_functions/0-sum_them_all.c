3ebb724fa8540562d46156b64b7dd6e0b7e4fac0
hlordzim <hlordzim.mh@gmail.com>
2023-11-09 14:13:56 -0800

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
 	unsigned int i;
 	int sum;
-	va_list args;
-
-	if (n == 0)
-	{
-		return (0);
-	}
-
-	va_start(args, n);
 
 	sum = 0;
+
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
