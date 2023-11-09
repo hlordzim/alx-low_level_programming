55f6fe24446b1178202b8450f6cf8403f9a31f82
hlordzim <hlordzim.mh@gmail.com>
2023-11-09 14:43:42 -0800

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
-#ifndef _VARIADIC_FUNCTION_H
-#define _VARIADIC_FUNCTION_H
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
+int _putchar(char);
 
 #endif
