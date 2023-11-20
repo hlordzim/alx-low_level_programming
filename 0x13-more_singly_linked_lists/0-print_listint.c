49ab93076e86119fd3cc534b3d5659b8fb9efa95
hlordzim <hlordzim.mh@gmail.com>
2023-11-20 14:48:35 -0800

    task 0 Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t element = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		element++;
+		h = h->next;
+	}
+	return (element);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# 0x13. C - More singly linked lists
+
+ Singly linked lists are used to create dynamic data structures that facilitate flexible and efficient data manipulation
