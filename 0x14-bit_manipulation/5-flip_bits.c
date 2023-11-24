7ecbfd3d7b43fa331548008d250b88118648d7ef
hlordzim <hlordzim.mh@gmail.com>
2023-11-24 12:06:48 -0800

    task5-bit_manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - number of bits needed to flip from one number to another.
+ * @n: first number
+ * @m: second number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a;
+	unsigned int count = 0;
+
+	for (a = 63; a >= 0; a--)
+	{
+		if (((n ^ m) >> a) & 1)
+			count++;
+	}
+	return (count);
+}
